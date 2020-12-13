3c244b67692a2bbd7c62c6926d9a936f922e856d
Nathan Floris Copier <nfcopier1@aggiemail.usu.edu>
2020-12-13 01:08:35 -0700

    Unify CLI

--- build-only.mjs
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env node
-import build from "./build.mjs";
-import buildOptions from "./build-options.mjs";
-
-const root = process.cwd();
-
-await build(root, buildOptions);
--- cli.mjs
+++ cli.mjs
@@ -3,14 +3,15 @@ import bumpVersion from "./bump-version.mjs";
 import {exec} from "child_process";
 import buildOptions from "./build-options.mjs";
 
-const publicFlags = process.argv.includes("--public") ? "--access public" : "";
+const root = process.cwd();
+await build(root, buildOptions);
+
+if (!process.argv.includes("--publish")) process.exit(0);
 
 const version = (process.env.version || "v0.1.0").slice(1);
-const root = process.cwd();
-
-await build(root, buildOptions);
 await bumpVersion(root, version, buildOptions);
 
+const publicFlags = process.argv.includes("--public") ? "--access public" : "";
 const command = `npm publish ./${buildOptions.outDir} ${publicFlags}`;
 const child = exec(command, {env: process.env});
 child.stdout.on("data", data => console.log(data.toString()));
